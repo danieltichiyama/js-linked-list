4af10f7261913ea54046a186c15c9b9b2e77887b
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-13 17:35:53 -1000

    finished, ready for review

--- linkedList.js
+++ linkedList.js
@@ -31,26 +31,74 @@ function linkedListGenerator() {
   };
 
   let get = function(num) {
-    let currentNode = head;
-    let nextNode = head.next;
     if (num === 0) {
       return head;
     }
+
+    let parentNode = head;
+    let childNode = parentNode.next;
     while (num > 1) {
-      if (nextNode.next === null) {
+      if (childNode.next === null) {
         return false;
       }
-      nextNode = nextNode.next;
-      currentNode = nextNode;
+      childNode = childNode.next;
       num--;
     }
 
-    return nextNode;
+    return childNode;
   };
 
-  let remove = function(num) {};
+  let remove = function(num) {
+    if (num === 0) {
+      head = head.next;
+      if (get(num) === false) {
+        return false;
+      }
+    } else {
+      let currentNode = get(num);
+      if (currentNode === false) {
+        return false;
+      }
 
-  let insert = function(val, num) {};
+      let previousNode = get(num - 1);
+
+      if (currentNode.next === null) {
+        previousNode.next = null;
+        currentNode = null;
+        tail = previousNode;
+        return;
+      }
+      previousNode.next = currentNode.next;
+      currentNode = previousNode.next;
+      tail = currentNode;
+    }
+  };
+
+  let insert = function(val, num) {
+    let newNode = {
+      value: val,
+      next: null
+    };
+
+    let currentNode = get(num);
+    let previousNode = get(num - 1);
+
+    if (num === 0) {
+      let headStorage = head;
+      newNode.next = headStorage;
+      head = newNode;
+    }
+    if (num < 0) {
+      return false;
+    }
+
+    if (get(num) === false) {
+      return false;
+    }
+
+    newNode.next = currentNode;
+    previousNode.next = newNode;
+  };
 
   return {
     getHead: getHead,
